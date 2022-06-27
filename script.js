8fd3687bb72791b93aaf501e6ec9d51acfeaa861
Tolu Joel <toluisjoel@gmail.com>
2022-06-27 04:46:10 +0100

    Switch Case

--- script.js
+++ script.js
@@ -1,54 +1,48 @@
 function calcRps(move) {
     let gameCharacters = { 'p': 'paper', 'r': 'rock', 's': 'scissors' },
-    chrValues = Object.values(gameCharacters),
-    computerMove = chrValues[Math.floor(Math.random() * chrValues.length)];
-    var computerScore = Number(document.getElementById('computerScore').innerHTML), playerScore = Number(document.getElementById('playerScore').innerHTML);
+        chrValues = Object.values(gameCharacters),
+        computerMove = chrValues[Math.floor(Math.random() * chrValues.length)];  // Get random character
+    var computerScore = Number(document.getElementById('computerScore').innerHTML),
+        playerScore = Number(document.getElementById('playerScore').innerHTML);
     document.getElementById('computerImage').src = `images/${computerMove}.svg`;
 
-    if (move == 'p') {
-        move = gameCharacters[move];
-        if (move == computerMove) {
-            document.querySelector('.vs').innerHTML = '🎀'
-        } else if (computerMove == 'rock') {
-            document.querySelector('.vs').innerHTML = 'VS'
-            document.getElementById('playerScore').innerHTML = playerScore + 1;
-        } else if (computerMove == 'scissors') {
-            document.querySelector('.vs').innerHTML = 'VS'
-            document.getElementById('computerScore').innerHTML = computerScore + 1;
-        }
+    switch (move) {
+        case 'p':
+            move = gameCharacters[move];
+            if (move == computerMove) {
+            } else if (computerMove == 'rock') {
+                document.getElementById('playerScore').innerHTML = playerScore + 1;
+            } else if (computerMove == 'scissors') {
+                document.getElementById('computerScore').innerHTML = computerScore + 1;
+            }
+            break;
 
-    } else if (move == 'r') {
-        move = gameCharacters[move];
+        case 'r':
+            move = gameCharacters[move];
 
-        if (move == computerMove) {
-            document.querySelector('.vs').innerHTML = '🎀'
-        } else if (computerMove == 'scissors') {
-            document.querySelector('.vs').innerHTML = 'VS'
-            document.getElementById('playerScore').innerHTML = playerScore + 1;
-        } else if (computerMove == 'paper') {
-            document.querySelector('.vs').innerHTML = 'VS'
-            document.getElementById('computerScore').innerHTML = computerScore + 1;
-        }
+            if (move == computerMove) {
+            } else if (computerMove == 'scissors') {
+                document.getElementById('playerScore').innerHTML = playerScore + 1;
+            } else if (computerMove == 'paper') {
+                document.getElementById('computerScore').innerHTML = computerScore + 1;
+            }
+            break;
 
-    } else if (move == 's') {
-        move = gameCharacters[move];
+        case 's':
+            move = gameCharacters[move];
 
-        if (move == computerMove) {
-            document.querySelector('.vs').innerHTML = '🎀'
-        } else if (computerMove == 'paper') {
-            document.querySelector('.vs').innerHTML = 'VS'
-            document.getElementById('playerScore').innerHTML = playerScore + 1;
-        } else if (computerMove == 'rock') {
-            document.querySelector('.vs').innerHTML = 'VS'
-            document.getElementById('computerScore').innerHTML = computerScore + 1;
-        }
+            if (move == computerMove) {
+            } else if (computerMove == 'paper') {
+                document.getElementById('playerScore').innerHTML = playerScore + 1;
+            } else if (computerMove == 'rock') {
+                document.getElementById('computerScore').innerHTML = computerScore + 1;
+            }
     }
 }
 
 function play() {
-
     const rockButton = document.querySelector('#rockButton');
-    rockButton.addEventListener('click', function (e) {
+    rockButton.addEventListener('click', (e) => {
         e.preventDefault();
         let playerMove = 'r';
         calcRps(playerMove)
@@ -56,18 +50,16 @@ function play() {
     });
 
     const paperButton = document.querySelector('#paperButton');
-    paperButton.addEventListener('click', function (e) {
+    paperButton.addEventListener('click', (e) => {
         let playerMove = 'p';
         calcRps(playerMove)
         document.getElementById('playerImage').src = 'images/paper.svg';
     });
 
     const scissorsButton = document.querySelector('#scissorsButton');
-    scissorsButton.addEventListener('click', function aba(e) {
+    scissorsButton.addEventListener('click', (e) => {
         let playerMove = 's';
         calcRps(playerMove)
         document.getElementById('playerImage').src = 'images/scissors.svg';
     });
 }
-
-// onload="playerName = Math.floor(Math.random() * players.length);document.getElementById('otherPlayer').innerHTML = players[playerName].toUpperCase()"
